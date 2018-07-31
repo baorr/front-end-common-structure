37ea39b969692912e41c5e5aef472e41beb9f40b
baobao <acbrr1985@163.com>
2018-07-31 22:09:33 +0800

    add swagger ui

--- swauger-ui.js
+++ swauger-ui.js
@@ -3,7 +3,7 @@ const pathToSwaggerUi = require('swagger-ui-dist').absolutePath()
 
 const app = express();
 
-app.use(express.static(pathToSwaggerUi));
+app.use(express.static('./swagger-ui/dist'));
 
 app.listen(4000, function () {
     console.log('Swauger app listening on port 4000!\n');
